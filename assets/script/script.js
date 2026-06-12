078356589512843b062b8c0ff96bc9fe29d96088
bkmzDJ <yugov2008@gmail.com>
2026-06-13 00:42:42 +0500

    мобильная версия сделана, если не нужно ниче исправлять то готово

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,12 @@
-//JAVA SCRIPT CODE:
+let bool = false
+let nav = document.getElementById('nav')
+document.getElementById('header').addEventListener('click', ()=>{
+    if(!bool){
+        nav.style.top = "9vh"
+        bool = true
+    } else if (bool){
+        nav.style.top = "-30vh"
+        bool = false
+    }
+     
+})
